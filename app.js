3aa83c6f2eab3f61693791aa67cec8d75d3c2ff3
kauzarc <45573975+kauzarc@users.noreply.github.com>
2020-07-14 19:17:51 +0200

    more style and redirect

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
+app.use(function (req, res, next) {
+  res.redirect("/");
+});
+
 var server = app.listen(8080, function () {
   console.log("Listening on port 8080");
 });
